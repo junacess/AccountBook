b6eecbaaf0f8180f6c76bf6e6284f57d147526c7
張君如 <a403176845643457@gmail.com>
2020-11-09 20:23:37 +0800

    no message

--- app/src/main/java/com/example/accountbook/MainActivity.java
+++ app/src/main/java/com/example/accountbook/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.accountbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.accountbook.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.fragment_account);
+
+//        Fragment fragment = new MainFragment();
+//        FragmentManager manager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+//        fragmentTransaction.add(R.id.fragmentLayout,fragment);
+//        fragmentTransaction.commit();
     }
 
     @Override
--- app/src/main/java/com/example/accountbook/main/MainFragment.java
+++ app/src/main/java/com/example/accountbook/main/MainFragment.java
@@ -17,9 +17,12 @@ public class MainFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main,container);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_main, container,false);
+        return view;
     }
+
+
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
